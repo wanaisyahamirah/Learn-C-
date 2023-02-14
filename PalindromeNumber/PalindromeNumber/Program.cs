5dadae6f2639653acca52980cd6e6f8674687bd7
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-14 08:38:07 +0800

    add method to get user input

--- PalindromeNumber/PalindromeNumber/Program.cs
+++ PalindromeNumber/PalindromeNumber/Program.cs
@@ -8,6 +8,12 @@ namespace PalindromeNumber
 {
     class Program
     {
+
+        /*
+            -- Wikipedia
+            A palindromic number (also known as a numeral palindrome or a numeric palindrome) 
+            is a number (such as 16461) that remains the same when its digits are reversed.
+        */
         static void Main(string[] args)
         {
             // The code provided will print ‘Hello World’ to the console.
@@ -17,5 +23,13 @@ namespace PalindromeNumber
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
+
+        static int GetUserInput()
+        {
+            Console.Write("Enter any number: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        
     }
 }
