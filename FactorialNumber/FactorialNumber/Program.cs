92366c3b15c71810cc0102ea3083cbd6dd4c69ea
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-15 08:45:34 +0800

    create method to get user input

--- FactorialNumber/FactorialNumber/Program.cs
+++ FactorialNumber/FactorialNumber/Program.cs
@@ -12,10 +12,18 @@ namespace FactorialNumber
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            int num = GetUserInput();
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
+
+        static int GetUserInput()
+        {
+            Console.Write("Enter any number: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
     }
 }
