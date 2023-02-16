6d610145e9c4227c94cc816425b63799bbe145bd
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-16 08:47:39 +0800

    completed program but wrong logic, revamp where needed

--- ArmstrongNumber/ArmstrongNumber/Program.cs
+++ ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -12,7 +12,10 @@ namespace ArmstrongNumber
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            int num = GetUserInput();
+            Boolean isArmStrong = IsArmstrongNumber(num);
+            PrintMessage(num, isArmStrong);
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
@@ -50,16 +53,18 @@ namespace ArmstrongNumber
             {
                 return true;
             }
-            else
-            {
-                return false;
-            }
+            return false;            
 
         }
 
-        static void PrintMessage(int inputNumber, int result)
+        static void PrintMessage(int inputNumber, Boolean result)
         {
-            Console.WriteLine("Factorial of " + inputNumber + " is " + result);
+            Console.WriteLine("result is " + result);
+            if(result)
+            {
+                Console.WriteLine(inputNumber + " is an Armstrong Number");
+            }
+            Console.WriteLine(inputNumber + " is not an Armstrong Number");
         }
     }
 }
