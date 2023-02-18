e06def7e4bfcd611c6bc6c048f9193763c7cc504
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-18 23:18:27 +0800

    implement caller in main method

--- DecimalToBinary/DecimalToBinary/Program.cs
+++ DecimalToBinary/DecimalToBinary/Program.cs
@@ -12,7 +12,10 @@ namespace DecimalToBinary
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            int decimalNum = GetUserInput();
+            String binaryNum = ConvertDecimalToBinary(decimalNum);
+            PrintMessage(decimalNum, binaryNum);
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
