1eafcd9873b424008dc1b368e80dffa89a70b7e4
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-19 11:53:21 +0800

    add comment and program completed

--- AlphabetTriangle/AlphabetTriangle/Program.cs
+++ AlphabetTriangle/AlphabetTriangle/Program.cs
@@ -27,6 +27,16 @@ namespace AlphabetTriangle
             return Convert.ToInt32(Console.ReadLine());
         }
 
+        /*
+         In the PrintAlphabetTriangle method, 
+         we have used three nested loops to print the spaces, 
+         the increasing sequence of letters, and the decreasing sequence of letters. 
+         The first loop iterates from 1 to the input num, 
+         and the inner loops print the spaces and the letters. 
+         The letters are printed by converting the loop variable to a character and 
+         adding 64 to get the corresponding uppercase letter (A = 65 in the ASCII table).
+         */
+
         static void PrintAlphabetTriangle(int num)
         {
             for (int i = 1; i <= num; i++)
