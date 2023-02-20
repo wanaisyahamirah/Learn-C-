5612363c46d95676cc3da532e23be249a49556ac
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-21 01:11:50 +0800

    implement caller in main method, program completed

--- FibonacciTriangle/FibonacciTriangle/Program.cs
+++ FibonacciTriangle/FibonacciTriangle/Program.cs
@@ -12,7 +12,9 @@ namespace FibonacciTriangle
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            int rows = GetUserInput();
+            PrintFibonacciTriangle(rows);
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
