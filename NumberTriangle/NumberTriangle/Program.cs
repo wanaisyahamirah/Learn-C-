99b78661e1cbdc43fe3121dc69b3a96012054662
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-20 08:00:38 +0800

    implement calling methods in main, program completed

--- NumberTriangle/NumberTriangle/Program.cs
+++ NumberTriangle/NumberTriangle/Program.cs
@@ -12,7 +12,11 @@ namespace NumberTriangle
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            Console.Write("Enter the number of rows: ");
+            int numRows = Convert.ToInt32(Console.ReadLine());
+
+            PrintNumberTriangle(numRows);
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
