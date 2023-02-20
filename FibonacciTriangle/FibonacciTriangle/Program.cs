e039783101c91e95ceafe1f0b3eab6e3a61203a4
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-21 01:08:47 +0800

    implement method PrintFibonacciTriangle()

--- FibonacciTriangle/FibonacciTriangle/Program.cs
+++ FibonacciTriangle/FibonacciTriangle/Program.cs
@@ -36,7 +36,14 @@ namespace FibonacciTriangle
 
         static void PrintFibonacciTriangle(int n)
         {
-
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(Fibonacci(j) + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
