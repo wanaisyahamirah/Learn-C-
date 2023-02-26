4709a3ceb697e7f43c94ec6af8b763e091cf8773
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-27 07:28:21 +0800

    create and implement Substract() method

--- PrintSpecifiedOperationsResult/PrintSpecifiedOperationsResult/Program.cs
+++ PrintSpecifiedOperationsResult/PrintSpecifiedOperationsResult/Program.cs
@@ -23,5 +23,10 @@ namespace PrintSpecifiedOperationsResult
             return num1 + num2;
         }
 
+        static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
     }
 }
