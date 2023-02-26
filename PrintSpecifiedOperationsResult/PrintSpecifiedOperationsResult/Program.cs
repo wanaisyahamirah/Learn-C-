7ee61d12c6f2c53856b3889593534327672a4d76
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-27 07:29:24 +0800

    create and implement Divide() method

--- PrintSpecifiedOperationsResult/PrintSpecifiedOperationsResult/Program.cs
+++ PrintSpecifiedOperationsResult/PrintSpecifiedOperationsResult/Program.cs
@@ -33,6 +33,11 @@ namespace PrintSpecifiedOperationsResult
             return num1 * num2;
         }
 
+        static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
 
     }
 }
