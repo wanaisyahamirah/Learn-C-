a0073f581d9cc2836b265488e5444b1a1c7375f1
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-26 21:28:15 +0800

    implement caller in the main method

--- BigToLittleEndian/BigToLittleEndian/Program.cs
+++ BigToLittleEndian/BigToLittleEndian/Program.cs
@@ -12,7 +12,17 @@ namespace BigToLittleEndian
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            
+            // Create a sample Big Endian byte array
+            byte[] bigEndianBytes = new byte[] { 0x12, 0x34, 0x56, 0x78 };
+
+            // Convert the Big Endian byte array to Little Endian
+            byte[] littleEndianBytes = ConvertBigEndianToLittleEndian(bigEndianBytes);
+
+            // Print the original and converted byte arrays
+            Console.WriteLine("Original Big Endian Bytes: " + BitConverter.ToString(bigEndianBytes));
+            Console.WriteLine("Converted Little Endian Bytes: " + BitConverter.ToString(littleEndianBytes));
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
