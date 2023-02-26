cdbe4ce9cdb725dc1fdbaf046a780b9fdc30dd4c
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-27 07:28:49 +0800

    create and implement Multiply() method

--- PrintSpecifiedOperationsResult/PrintSpecifiedOperationsResult/Program.cs
+++ PrintSpecifiedOperationsResult/PrintSpecifiedOperationsResult/Program.cs
@@ -28,5 +28,11 @@ namespace PrintSpecifiedOperationsResult
             return num1 - num2;
         }
 
+        static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+
     }
 }
