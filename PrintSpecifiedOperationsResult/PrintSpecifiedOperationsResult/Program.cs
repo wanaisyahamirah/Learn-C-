7bf464bc6c19ffd8ba938ad84f5d89394b4f77de
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-27 07:31:52 +0800

    revamp method to handle division by zero

--- PrintSpecifiedOperationsResult/PrintSpecifiedOperationsResult/Program.cs
+++ PrintSpecifiedOperationsResult/PrintSpecifiedOperationsResult/Program.cs
@@ -35,7 +35,15 @@ namespace PrintSpecifiedOperationsResult
 
         static int Divide(int num1, int num2)
         {
-            return num1 / num2;
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error: division by zero");
+                return 0;
+            }
+            else
+            {
+                return num1 / num2;
+            }
         }
 
         static int Modulus(int num1, int num2)
