47314a4cc1572552ec159ff065f40a9bd7dd2b03
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-02-28 07:00:47 +0800

    implement caller in the main method

--- AverageNumber/AverageNumber/Program.cs
+++ AverageNumber/AverageNumber/Program.cs
@@ -12,7 +12,11 @@ namespace AverageNumber
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            int[] numbers = GetIntegerUserInput();
+            double average = CalculateAverage(numbers);
+            Console.WriteLine("The average is: " + average);
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
