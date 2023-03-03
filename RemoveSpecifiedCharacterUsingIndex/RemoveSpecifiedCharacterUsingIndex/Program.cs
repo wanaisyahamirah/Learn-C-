c300a4436ebc67ef98acec03d3fd1ca783911501
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-04 00:48:11 +0800

    implement caller in the main method

--- RemoveSpecifiedCharacterUsingIndex/RemoveSpecifiedCharacterUsingIndex/Program.cs
+++ RemoveSpecifiedCharacterUsingIndex/RemoveSpecifiedCharacterUsingIndex/Program.cs
@@ -12,7 +12,17 @@ namespace RemoveSpecifiedCharacterUsingIndex
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            
+            // Get user input
+            string inputString = GetUserInput("Enter a string: ");
+            int charIndex = GetPositiveNumber("Enter the index of the character to remove: ");
+
+            // Remove character by index
+            string outputString = RemoveCharByIndex(inputString, charIndex);
+
+            // Print the result
+            PrintResult(outputString);
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
