fcc1c3c230b62a7e219273354e7ed76493e293f3
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-05 08:28:32 +0800

    implement caller, program completed

--- ConvertStringToLowercase/ConvertStringToLowercase/Program.cs
+++ ConvertStringToLowercase/ConvertStringToLowercase/Program.cs
@@ -12,7 +12,12 @@ namespace ConvertStringToLowercase
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            string input = GetInputString();
+
+            string lowercase = ConvertToLowercase(input);
+
+            PrintResult(lowercase);
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
