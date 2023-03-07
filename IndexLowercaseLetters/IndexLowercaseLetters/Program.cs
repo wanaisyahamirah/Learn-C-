7a9cf87e3496a2c0a2d4a23684b11f2b98632319
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-08 07:17:04 +0800

    implement caller of GetInputString()

--- IndexLowercaseLetters/IndexLowercaseLetters/Program.cs
+++ IndexLowercaseLetters/IndexLowercaseLetters/Program.cs
@@ -12,7 +12,8 @@ namespace IndexLowercaseLetters
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            String inputString = GetStringInput();
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
