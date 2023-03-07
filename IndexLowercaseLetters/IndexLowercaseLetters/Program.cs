c5743bfa205663ce075d54441f925d2083c4f1b6
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-08 07:18:53 +0800

    impelement caller in main method of GetLowerCaseIndices

--- IndexLowercaseLetters/IndexLowercaseLetters/Program.cs
+++ IndexLowercaseLetters/IndexLowercaseLetters/Program.cs
@@ -14,6 +14,9 @@ namespace IndexLowercaseLetters
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
 
             String inputString = GetStringInput();
+
+            int[] indices = GetLowerCaseIndices(str);
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
