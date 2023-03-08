9921e7c39d8c18da2bbe9a0dfe008325ca71fc29
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-09 07:16:24 +0800

    implement method GetPostfix()

--- GetCenturyFromYear/GetCenturyFromYear/Program.cs
+++ GetCenturyFromYear/GetCenturyFromYear/Program.cs
@@ -40,7 +40,32 @@ namespace GetCenturyFromYear
 
         static string GetPostfix(int century)
         {
-
+            string postfix;
+            int lastTwoDigits = century % 100;
+            if (lastTwoDigits >= 11 && lastTwoDigits <= 13)
+            {
+                postfix = "th";
+            }
+            else
+            {
+                int lastDigit = lastTwoDigits % 10;
+                switch (lastDigit)
+                {
+                    case 1:
+                        postfix = "st";
+                        break;
+                    case 2:
+                        postfix = "nd";
+                        break;
+                    case 3:
+                        postfix = "rd";
+                        break;
+                    default:
+                        postfix = "th";
+                        break;
+                }
+            }
+            return postfix;
         }
 
         static void PrintResult(int year, int century)
