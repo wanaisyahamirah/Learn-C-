565d240507853f910919bc8a22c6fbf935e21dfd
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-11 23:08:39 +0800

    implement method IsPrime()

--- SumFirstFiveHndredPrimeNumbuers/SumFirstFiveHndredPrimeNumbuers/Program.cs
+++ SumFirstFiveHndredPrimeNumbuers/SumFirstFiveHndredPrimeNumbuers/Program.cs
@@ -20,7 +20,14 @@ namespace SumFirstFiveHndredPrimeNumbuers
 
         static bool IsPrime(int number)
         {
-
+            if (number < 2) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
         }
     }
 }
