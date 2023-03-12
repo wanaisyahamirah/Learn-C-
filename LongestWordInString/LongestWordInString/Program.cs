8e853672a5e8acf434bdc8ff313529bd18d4cad2
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-12 18:00:41 +0800

    implement caller in main, program completed

--- LongestWordInString/LongestWordInString/Program.cs
+++ LongestWordInString/LongestWordInString/Program.cs
@@ -12,7 +12,13 @@ namespace LongestWordInString
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            string aString = GetInput();
+
+            string longestWord = FindLongestWord(aString);
+
+            PrintResult(longestWord);
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
