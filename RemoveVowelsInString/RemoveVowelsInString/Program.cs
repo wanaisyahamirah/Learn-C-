bd1ac9b60cc1fd11ea5c23386f03d8e741476414
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-14 07:29:52 +0800

    implement caller, program completed

--- RemoveVowelsInString/RemoveVowelsInString/Program.cs
+++ RemoveVowelsInString/RemoveVowelsInString/Program.cs
@@ -12,7 +12,10 @@ namespace RemoveVowelsInString
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            String input = GetInput();
+            string result = RemoveVowels(input);
+            PrintResult(result);
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
