116f8d8ee2d1ad741b74382285117cc24372759e
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-15 07:28:13 +0800

    implement caller in main, program completed

--- GenerateNewStringOfOthers/GenerateNewStringOfOthers/Program.cs
+++ GenerateNewStringOfOthers/GenerateNewStringOfOthers/Program.cs
@@ -12,7 +12,16 @@ namespace GenerateNewStringOfOthers
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            
+            // Get input string
+            string input = GetInput();
+
+            // Create new string of every other character
+            string result = GetEveryOtherCharacter(input);
+
+            // Print result
+            PrintResult(result);
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
