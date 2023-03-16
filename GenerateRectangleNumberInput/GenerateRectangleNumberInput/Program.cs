7097cfeeb2cd521c64aa246287ba03836395e906
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-17 00:39:41 +0800

    add method GetInput()

--- GenerateRectangleNumberInput/GenerateRectangleNumberInput/Program.cs
+++ GenerateRectangleNumberInput/GenerateRectangleNumberInput/Program.cs
@@ -17,5 +17,10 @@ namespace GenerateRectangleNumberInput
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
+
+        static int GetInput()
+        {
+
+        }
     }
 }
